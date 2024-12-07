a84d0f37196ccf19536366d56539f121967b9ce4
Blake Wingard <bats23456789@gmail.com>
2024-12-06 23:44:03 -0600

    Completed Day7 Python

---
+++ 2024/python/Day07/Part2.py
@@ -0,0 +1,58 @@
+import sys
+
+
+def passing(test, values):
+    new_vals = []
+
+    if len(values) == 1:
+        return test == values[0]
+
+    new_vals.append(values[0] + values[1])
+    if len(values) > 2:
+        for value in values[2:]:
+            new_vals.append(value)
+
+    if passing(test, new_vals):
+        return True
+
+    new_vals = []
+    new_vals.append(values[0] * values[1])
+    if len(values) > 2:
+        for value in values[2:]:
+            new_vals.append(value)
+
+    if passing(test, new_vals):
+        return True
+
+    new_vals = []
+    new_vals.append(int(str(values[0]) + str(values[1])))
+    if len(values) > 2:
+        for value in values[2:]:
+            new_vals.append(value)
+
+    if passing(test, new_vals):
+        return True
+
+    return False
+
+
+def main():
+    file = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+    data = open(file).read().strip()
+
+    total = 0
+
+    for line in data.split('\n'):
+        test, values = line.split(': ')
+        test = int(test)
+        values = list(map(int, values.split(' ')))
+
+        if passing(test, values):
+            total += test
+
+    print(total)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
