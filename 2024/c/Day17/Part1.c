26e4c4b9b9a6028b2504151f13d43fc7f508f34a
Blake Wingard <bwingard@IBML.local>
2024-12-19 12:14:35 -0600

    Completed Day17 C

--- 2024/c/Day17/Part1.c
+++ 2024/c/Day17/Part1.c
@@ -1,14 +1,27 @@
 /*
  * Name: Part1.c
  * Auth: Blake Wingard - bats23456789@gmail.com
- * Desc: Part 1 for day 1 of AOC.
- * Vers: 1.0.0 12/01/2024 CBW - Original code.
+ * Desc: Part 1 for day 17 of AOC.
+ * Vers: 1.0.0 12/17/2024 CBW - Original code.
  */
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+int get_combo(int val, int regA, int regB, int regC);
+
 int main(int argc, char **argv) {
     FILE *file;
+	int regA;
+	int regB;
+	int regC;
+	int *program;
+	size_t p_sz = 0;
+	size_t p_max = 100;
+	int *out;
+	size_t out_sz = 0;
+	size_t out_max = 100;
+	int block;
 
     if (argc < 2) {
         fprintf(stderr, "ERROR: Not enough inputs\n");
@@ -21,5 +34,91 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
+	fscanf(file, "Register A: %d\nRegister B: %d\nRegister C: %d\n\nProgram: ",
+		&regA,
+		&regB,
+		&regC);
+
+	program = malloc(sizeof *program * p_max);
+
+	while (fscanf(file, "%d,", &block) != EOF) {
+		program[p_sz++] = block;
+		if (p_sz >= p_max) {
+			p_max *= 2;
+			program = realloc(program, sizeof *program * p_max);
+		}
+	}
+
+	fclose(file);
+
+	if (p_max > p_sz) {
+		p_max = p_sz;
+		program = realloc(program, sizeof *program * p_max);
+	}
+
+	out = malloc(sizeof *out * out_max);
+
+	for (ssize_t i = 0; i < p_sz; ++i) {
+		int op = program[i++];
+		int val = program[i];
+
+		switch (op) {
+			case 0:
+				regA = regA / pow(2, get_combo(val, regA, regB, regC));
+				break;
+			case 1:
+				regB = regB ^ val;
+				break;
+			case 2:
+				regB = get_combo(val, regA, regB, regC) % 8;
+				break;
+			case 3:
+				if (regA != 0) {
+					i = val - 1;
+ 				}
+				break;
+			case 4:
+				regB = regB ^ regC;
+				break;
+			case 5:
+				out[out_sz++] = get_combo(val, regA, regB, regC) % 8;
+				if (out_sz >= out_max) {
+					out_max *= 2;
+					out = realloc(out, sizeof *out * out_max);
+				}
+				break;
+			case 6:
+				regB = regA / pow(2, get_combo(val, regA, regB, regC));
+				break;
+			case 7:
+				regC = regA / pow(2, get_combo(val, regA, regB, regC));
+				break;
+		}
+	}
+
+	for (size_t i = 0; i < out_sz; ++i) {
+		putchar(out[i] + '0');
+		putchar(i + 1 == out_sz ? '\n' : ',');
+	}
+
+	free(out);
+	free(program);
+
     return EXIT_SUCCESS;
+}
+
+int get_combo(int val, int regA, int regB, int regC) {
+	switch (val) {
+		case 4:
+			return regA;
+			break;
+		case 5:
+			return regB;
+		case 6:
+			return regC;
+			break;
+		default:
+			return val;
+			break;
+	}
 }
--- 2024/c/Day17/Part2.c
+++ 2024/c/Day17/Part2.c
@@ -1,14 +1,30 @@
 /*
- * Name: Part1.c
+ * Name: Part2.c
  * Auth: Blake Wingard - bats23456789@gmail.com
- * Desc: Part 1 for day 1 of AOC.
- * Vers: 1.0.0 12/01/2024 CBW - Original code.
+ * Desc: Part 2 for day 17 of AOC.
+ * Vers: 1.0.0 12/17/2024 CBW - Original code.
  */
+#include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
+
+int64_t get_values(int64_t *program, size_t sz, int64_t regA, int64_t regB, int64_t regC);
+int64_t get_combo(int64_t val, int64_t regA, int64_t regB, int64_t regC);
 
 int main(int argc, char **argv) {
     FILE *file;
+	int64_t regA;
+	int64_t regB;
+	int64_t regC;
+	int64_t *program;
+	size_t p_sz = 0;
+	size_t p_max = 100;
+	int64_t *out;
+	size_t out_sz = 0;
+	size_t out_max = 100;
+	int64_t block;
 
     if (argc < 2) {
         fprintf(stderr, "ERROR: Not enough inputs\n");
@@ -21,5 +37,121 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
+	fscanf(file, "Register A: %"SCNi64"\nRegister B: %"SCNi64"\nRegister C: %"SCNi64"\n\nProgram: ",
+		&regA,
+		&regB,
+		&regC);
+
+	program = malloc(sizeof *program * p_max);
+
+	while (fscanf(file, "%"SCNi64",", &block) != EOF) {
+		program[p_sz++] = block;
+		if (p_sz >= p_max) {
+			p_max *= 2;
+			program = realloc(program, sizeof *program * p_max);
+		}
+	}
+
+	fclose(file);
+
+	if (p_max > p_sz) {
+		p_max = p_sz;
+		program = realloc(program, sizeof *program * p_max);
+	}
+
+	out = malloc(sizeof *out * out_max);
+
+	regA = get_values(program, p_sz, 0, regB, regC);
+
+	printf("%"PRIi64"\n", regA);
+
+	for (ssize_t i = 0; i < p_sz; ++i) {
+		int64_t op = program[i++];
+		int64_t val = program[i];
+
+		switch (op) {
+			case 0:
+				regA = regA / pow(2, get_combo(val, regA, regB, regC));
+				break;
+			case 1:
+				regB = regB ^ val;
+				break;
+			case 2:
+				regB = get_combo(val, regA, regB, regC) % 8;
+				break;
+			case 3:
+				if (regA != 0) {
+					i = val - 1;
+ 				}
+				break;
+			case 4:
+				regB = regB ^ regC;
+				break;
+			case 5:
+				out[out_sz++] = get_combo(val, regA, regB, regC) % 8;
+				if (out_sz >= out_max) {
+					out_max *= 2;
+					out = realloc(out, sizeof *out * out_max);
+				}
+				break;
+			case 6:
+				regB = regA / pow(2, get_combo(val, regA, regB, regC));
+				break;
+			case 7:
+				regC = regA / pow(2, get_combo(val, regA, regB, regC));
+				break;
+		}
+	}
+
+	for (size_t i = 0; i < out_sz; ++i) {
+		if (program[i] != out[i]) {
+			fprintf(stderr, "ERROR: %"PRIi64" != %"PRIi64"\n", program[i], out[i]);
+			return EXIT_FAILURE;
+		}
+	}
+
+	free(out);
+	free(program);
+
     return EXIT_SUCCESS;
+}
+
+int64_t get_values(int64_t *program, size_t sz, int64_t regA, int64_t regB, int64_t regC) {
+	int64_t chunk = 0;
+
+	if (sz == 0) {
+		return regA;
+	}
+
+	chunk = program[--sz];
+
+	for (size_t i = 0; i < 8; ++i) {
+		int64_t tmp = (regA << 3) + i;
+		int64_t check = ((i ^ 4) ^ (tmp >> (i ^ 1))) % 8;
+
+		if (check == chunk) {
+			int64_t val = get_values(program, sz, tmp, regB, regC);
+			if (val >= 0) {
+				return val;
+			}
+		}
+	}
+
+	return -1;
+}
+
+int64_t get_combo(int64_t val, int64_t regA, int64_t regB, int64_t regC) {
+	switch (val) {
+		case 4:
+			return regA;
+			break;
+		case 5:
+			return regB;
+		case 6:
+			return regC;
+			break;
+		default:
+			return val;
+			break;
+	}
 }
